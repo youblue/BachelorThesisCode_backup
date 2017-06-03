499f8b6921b3c1a384b5617850492767db75e7b4
wszhang <sophiazws@gmail.com>
2017-06-03 15:04:16 -0700

    Add files via upload

---
+++ Resource.h
@@ -0,0 +1,59 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by BiYeSheJi.rc
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_BIYESHTYPE                  129
+#define IDD_BIYESHEJI_DIALOG            130
+#define IDC_Startcap                    1000
+#define IDC_ImageProcess                1001
+#define IDC_Stopcap                     1003
+#define IDC_VIEW                        1006
+#define IDC_EDIT1                       1007
+#define IDC_CurrentPosition             1007
+#define IDC_xStartLeft                  1007
+#define IDC_EDIT2                       1008
+#define IDC_xTurn1                      1008
+#define IDC_yStartLeft                  1008
+#define IDC_EDIT3                       1009
+#define IDC_CurrentDirection            1009
+#define IDC_xStartRight                 1009
+#define IDC_EDIT4                       1010
+#define IDC_TurnDirection               1010
+#define IDC_yStartRight                 1010
+#define IDC_EDIT5                       1011
+#define IDC_EDIT6                       1012
+#define IDC_yTurn1                      1013
+#define IDC_yTurn2                      1014
+#define IDC_xLeft1                      1014
+#define IDC_yLeft1                      1015
+#define IDC_xRight1                     1016
+#define IDC_yRight1                     1017
+#define IDC_GO                          1018
+#define IDC_K1                          1018
+#define IDC_RIGHT                       1019
+#define IDC_b1                          1019
+#define IDC_BACK                        1020
+#define IDC_K2                          1020
+#define IDC_LEFT                        1021
+#define IDC_b2                          1021
+#define IDC_STOP                        1022
+#define IDC_K                           1022
+#define IDC_b                           1023
+#define IDC_Theta                       1024
+#define IDC_le                          1025
+#define IDC_MX0                         1026
+#define IDC_MY0                         1027
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        136
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1030
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
